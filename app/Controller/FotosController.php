61d58eb148c9b8a5d14fb0c28c98f5c694627766
Paulo Victor <paulo@paulowd.com.br>
2015-02-17 01:19:12 -0200

    Criação do método add

--- app/Controller/FotosController.php
+++ app/Controller/FotosController.php
@@ -2,4 +2,30 @@
 
 class FotosController extends AppController {
 	public $scaffold = 'admin1910';
+
+	public function admin1910_add(){
+		if(!empty($this->request->data && $this->request->is('post'))){
+			if($this->Foto->createWithAttachments($this->request->data)){
+	            $this->Session->setFlash('Foto cadastrada com sucesso.', 'default', array('class'=>'success'));
+	            $this->redirect(array('action' => 'index'));
+	        } else {
+	        	$this->Session->setFlash('Não foi possível cadastrar a foto.');
+	        	$this->set('validationErrorsArray', $this->Foto->invalidFields());
+	            $this->redirect(array('action' => 'view', $id));
+	        }
+		}
+
+    	$status = $this->Foto->Post->Status->find(
+			'list', 
+			array(
+    			'conditions' => array('model' => 'Post')
+    		)
+    	);
+		$this->set(compact('status'));
+
+    	$categorias = $this->Foto->Post->Categoria->find('list');
+		$this->set(compact('categorias'));
+    	$campeonatos = $this->Foto->Post->Campeonato->find('list');
+		$this->set(compact('campeonatos'));
+	}
 }
--- app/Controller/VideosController.php
+++ app/Controller/VideosController.php
@@ -3,4 +3,31 @@
 class VideosController extends AppController {
 	
 	public $scaffold = 'admin1910';
+
+	public function admin1910_add(){
+		if(!empty($this->request->data && $this->request->is('post'))){
+			if($this->Video->createWithAttachments($this->request->data)){
+	            $this->Session->setFlash('Video cadastrado com sucesso.', 'default', array('class'=>'success'));
+	            $this->redirect(array('action' => 'index'));
+	        } else {
+	        	$this->Session->setFlash('Não foi possível cadastrar o vídeo.');
+	        	$this->set('validationErrorsArray', $this->Video->invalidFields());
+	        }
+		}
+
+    	$status = $this->Video->Post->Status->find(
+			'list', 
+			array(
+    			'conditions' => array('model' => 'Post')
+    		)
+    	);
+		$this->set(compact('status'));
+
+    	$categorias = $this->Video->Post->Categoria->find('list');
+		$this->set(compact('categorias'));
+    	$campeonatos = $this->Video->Post->Campeonato->find('list');
+		$this->set(compact('campeonatos'));
+    	$players = $this->Video->Player->find('list');
+		$this->set(compact('players'));
+	}
 }
